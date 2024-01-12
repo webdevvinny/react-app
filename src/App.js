b2373565a589b55c3422ca7559d3bc02b7c849e1
The Programmer <whiteangel@duck.com>
2024-01-12 17:45:36 +0300

    Making Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+// function call(){
+//   return(
+//     <h3>My Name Is a Programmer. Working with reactJs</h3>
+
+//   )
+// }
+
+const mine = () => {
+  return(
+    <>
+    <h1>I am  a Front-End Developer</h1>
+    <h1>Working With react Js</h1>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <mine />
     </div>
   );
 }
